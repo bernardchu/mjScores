bb840e25ece449fb24c514b0129d90fb1239ce77
Bernard Chu <bernard.chu@yahoo.com>
2016-02-27 11:51:01 -0800

    Add/remove active players.

--- main/mainController.js
+++ main/mainController.js
@@ -1,2 +1,45 @@
-function MainController() {
+function MainController( $mdSidenav ) {
+  this.players = [];
+  this.activePlayers = [];
+  this.$mdSidenav = $mdSidenav;
+  this.generateRandomPlayers( 10 );
 }
+
+MainController.prototype.generateRandomPlayers = function ( count ) {
+  while (count) {
+    this.players.push( {
+      name: this.names[ Math.floor( Math.random() * 10 ) ] + this.names[ Math.floor(Math.random() * 10) ],
+      points: Math.floor( Math.random() * 2000 ),
+      active: false
+    });
+    count--;
+  }
+}
+
+MainController.prototype.toggleRight = function () {
+  this.$mdSidenav( 'playerList' ).toggle();
+}
+
+MainController.prototype.togglePlayer = function ( player ) {
+  player.active = !player.active;
+  var index = this.activePlayers.indexOf( player );
+  if ( index === -1 ) {
+    if ( this.activePlayers.length === 4 ) { return; }
+    this.activePlayers.push( player );
+  } else {
+    this.activePlayers.splice( index, 1 );
+  }
+}
+
+MainController.prototype.names = [
+  'foo',
+  'bar',
+  'baz',
+  'monkey',
+  'banana',
+  'bread',
+  'face',
+  'head',
+  'poop',
+  'pants'
+];
